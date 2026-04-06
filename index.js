289f9b857c2d5c3a62d848662e33a7ebc6998e19
Dennis Abizar <denisabizar54@gmail.com>
2026-04-06 17:29:09 +0700

    fix :  allowed url cors

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-//require modul
+
 const userController = require("./src/user/user.controller");
 const authController = require("./src/auth/auth.controller");
 const absenController = require("./src/absen/absen.controller");
-const {verifyAdmin,verifyToken} = require("./src/middleware/auth.middleware");
-//init app
+const { verifyAdmin, verifyToken } = require("./src/middleware/auth.middleware");
+
+dotenv.config();
+
 const app = express();
-//init json
-app.use(cors());
+
 app.use(cors({
     origin: "https://attendify-blush.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
+
 app.use(express.json());
 
-//config dotenv
-dotenv.config();
-const PORT = process.env.PORT;
-
-
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("server berjalan");
 });
 
-//route
-app.use("/auth",authController);
-app.use("/user",verifyAdmin,userController);
-app.use("/absen",verifyToken,absenController);
+app.use("/auth", authController);
+app.use("/user", verifyAdmin, userController);
+app.use("/absen", verifyToken, absenController);
 
-//port
 module.exports = app;
